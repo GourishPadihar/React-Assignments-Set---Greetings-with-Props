8c794cbf0edffa169d7f49dbdea144c0baaabf7f
GourishPadihar <63400249+GourishPadihar@users.noreply.github.com>
2020-12-08 12:50:08 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from "react";
+
+function greeting(props){
+  const name=props.name;
+return(
+<>
+  <h1>Hey {}!</h1>
+  <h2>Welcome to Newton School.</h2>
+  </>
+);
+}
+export greeting;
